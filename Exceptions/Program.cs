5d89dbdeb590a97c221c2caa4a7a63fca95a69e7
justnusya <sahaidak.yana1123@vu.cdu.edu.ua>
2024-11-18 11:35:41 +0200

    Added exceptions for: file not found and some invalid input

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -11,26 +11,42 @@ namespace Exceptions
     {
         static void Main(string[] args)
         {
-            try
+            for (int i = 10; i < 30; i++)
             {
-                string line1, line2;
-                for(int i=10; i < 30; i++)
+                try
                 {
+                    int line1, line2;
                     using (StreamReader reader = new StreamReader($"{i}.txt"))
                     {
-                        line1 = reader.ReadLine();
-                        line2 = reader.ReadLine();
+                        line1 = int.Parse(reader.ReadLine());
+                        line2 = int.Parse(reader.ReadLine());
                     }
                     Console.WriteLine(line1);
-                    //Console.WriteLine(line2);
-                    Console.ReadLine();
+                    Console.WriteLine(line2);
                 }
-                
+                catch (FileNotFoundException)
+                {
+                    using (StreamWriter writer = new StreamWriter("no_file.txt", true))
+                    {
+                        writer.WriteLine($"{i}.txt");
+                    }
+                }
+                catch (FormatException)
+                {
+                    using (StreamWriter writer2 = new StreamWriter("bad_data.txt", true))
+                    {
+                        writer2.WriteLine($"{i}.txt");
+                    }
+                }
+                catch (ArgumentNullException)
+                {
+                    using (StreamWriter writer2 = new StreamWriter("bad_data.txt", true))
+                    {
+                        writer2.WriteLine($"{i}.txt");
+                    }
+                }
             }
-            catch (Exception)
-            {
-
-            }
+            Console.ReadKey();
         }
     }
 }
