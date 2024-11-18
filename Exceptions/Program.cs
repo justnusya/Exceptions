5d16e7e3b88551c3be1d712daca80fd3483d7a32
justnusya <sahaidak.yana1123@vu.cdu.edu.ua>
2024-11-18 10:17:11 +0200

    Add project files.

---
+++ Exceptions/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string line1, line2;
+                for(int i=10; i < 30; i++)
+                {
+                    using (StreamReader reader = new StreamReader($"{i}.txt"))
+                    {
+                        line1 = reader.ReadLine();
+                        line2 = reader.ReadLine();
+                    }
+                    Console.WriteLine(line1);
+                    //Console.WriteLine(line2);
+                    Console.ReadLine();
+                }
+                
+            }//
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
